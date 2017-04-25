9b4ba5925df3fa09904aa682e16e65a1089b42b8
Anang Dista Satria <anang-home@anang-homes-MacBook-Pro.local>
2017-04-26 05:47:01 +0700

    Small refactor
    Small refactor

--- Assets/TextController.cs
+++ Assets/TextController.cs
@@ -16,7 +16,8 @@ public class TextController : MonoBehaviour {
 		cell_mirror,
 		sheets_1,
 		lock_1,
-		freedom
+		freedom,
+		corridor_0
 	};
 
 	private GameStates currentState;
@@ -29,17 +30,17 @@ public class TextController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		print (currentState);
-		if (currentState == GameStates.cell) 			{state_cell();}
-		else if (currentState == GameStates.sheets_0) 	{state_sheets_0();}
-		else if (currentState == GameStates.sheets_1) 	{state_sheets_1();}
-		else if (currentState == GameStates.lock_0) 		{state_lock_0();}
-		else if (currentState == GameStates.lock_1) 		{state_lock_1();}
-		else if (currentState == GameStates.mirror) 		{state_mirror();}
-		else if (currentState == GameStates.cell_mirror) {state_cell_mirror();}
-		else if (currentState == GameStates.freedom) 	{state_freedom();}
+		if (currentState == GameStates.cell) 			{cell();}
+		else if (currentState == GameStates.sheets_0) 	{sheets_0();}
+		else if (currentState == GameStates.sheets_1) 	{sheets_1();}
+		else if (currentState == GameStates.lock_0) 		{lock_0();}
+		else if (currentState == GameStates.lock_1) 		{lock_1();}
+		else if (currentState == GameStates.mirror) 		{mirror();}
+		else if (currentState == GameStates.cell_mirror) {cell_mirror();}
+		else if (currentState == GameStates.corridor_0) 	{corridor_0();}
 	}
 
-	void state_cell(){
+	void cell(){
 		text.text = "You are in a prison cell, and you want to escape. There are " +
 			"some dirty sheets on the bed, a mirror on the wall, and the door " +
 			"is locked from the outside\n\n" +
@@ -56,7 +57,7 @@ public class TextController : MonoBehaviour {
 		}
 	}
 
-	void state_sheets_0(){
+	void sheets_0(){
 		text.text = "You can't believe you sleep in these thing! Surely it's " +
 			"time somebody changed them. The pleasure of prison life " +
 			"I guess!\n\n" +
@@ -67,14 +68,14 @@ public class TextController : MonoBehaviour {
 		}
 	}
 
-	void state_mirror() {
+	void mirror() {
 		text.text = "The dirty old mirror on the wall seems loose.\n\n" +
 					"Press T to Take the mirror, or R to Return to cell" ;
 		if (Input.GetKeyDown(KeyCode.T)) 		{currentState = GameStates.cell_mirror;}
 		else if (Input.GetKeyDown(KeyCode.R)) 	{currentState = GameStates.cell;}
 	}
 
-	void state_cell_mirror() {
+	void cell_mirror() {
 		text.text = "You are still in your cell, and you STILL want to escape! There are " +
 					"some dirty sheets on the bed, a mark where the mirror was, " +
 					"and that pesky door is still there, and firmly locked!\n\n" +
@@ -86,7 +87,7 @@ public class TextController : MonoBehaviour {
 		}
 	}
 
-	void state_lock_0(){
+	void lock_0(){
 		text.text = "This is one of those button locks. You have the idea what the " +
 					"combination  is. You wish you could somehow see where the dirty " +
 					"fingerprints were, maybe that would help.\n\n" +
@@ -97,7 +98,7 @@ public class TextController : MonoBehaviour {
 		}
 	}
 
-	void state_sheets_1(){
+	void sheets_1(){
 		text.text = "Holding a mirror in your hand doesn't make the sheets look "+
 					"any better.\n\n" +
 					"Press R to Return to roaming you cell";
@@ -106,22 +107,22 @@ public class TextController : MonoBehaviour {
 		}
 	}
 
-	void state_lock_1(){
+	void lock_1(){
 		text.text = "You carefully put the mirror through the bars and turn it round " +
 					"so you can see the lock. You can just make out fingerprints around " +
 					"the buttons. You press the dirty buttons, and hear a click.\n\n" +
 					"Press 0 to Open, or R to Return to your Cell.";
 
 		if(Input.GetKeyDown(KeyCode.Alpha0)){
-			currentState = GameStates.freedom;
+			currentState = GameStates.corridor_0;
 		}
 		if(Input.GetKeyDown(KeyCode.R)){
 			currentState = GameStates.cell_mirror;
 		}
 	}
 
-	void state_freedom() {
-		text.text = "You are FREE!\n\n" +
+	void corridor_0() {
+		text.text = "You are in a corridor now!\n\n" +
 			"Press P to Play again";
 		if (Input.GetKeyDown(KeyCode.P)) 		{currentState = GameStates.cell;}
 	}
